65a3af44dd188e658186c3d5ce9f8a56c7161029
vijay <vijaygupta701@gmail.com>
2025-08-03 14:23:23 +0530

    fix order section

--- src/Modals/order.modal.js
+++ src/Modals/order.modal.js
@@ -2,6 +2,11 @@ const mongoose = require('mongoose');
 
 const orderSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  name: { type: String },
+  pickupBy: { type: String },
+  phone: { type: String },
+  pickupDate: { type: String },
+  dropDate: { type: String },
   equipment: [{
     item: { type: mongoose.Schema.Types.ObjectId, ref: 'Stock' },
     qty: { type: Number, default: 1 },
@@ -9,6 +14,11 @@ const orderSchema = new mongoose.Schema({
     days: { type: Number, required: true },
     status: { type: String, enum: ['Rented Out', 'Returned'], default: 'Rented Out' }
   }],
+  totalAmount: { type: Number },
+  totalBalance: { type: Number },
+  paid: { type: String },
+  discount: { type: String },
+  outstanding: { type: Number },
 }, { strict: false });
 
 const Order = mongoose.model('Order', orderSchema);
